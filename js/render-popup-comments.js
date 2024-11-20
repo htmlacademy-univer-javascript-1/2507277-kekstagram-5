b2b8728d1e75a06bc1e075e8471c497c6238e4de
Jewel <zensot1@yandex.ru>
2024-11-20 19:52:54 +0500

    постепенный вывод комментариев по 5

--- js/render-popup-comments.js
+++ js/render-popup-comments.js
@@ -1,23 +1,44 @@
 export const renderPopupComments = (photo) => {
   const commentsList = document.querySelector('.social__comments');
+  const commentCount = document.querySelector('.social__comment-count');
+  const loadMoreButton = document.querySelector('.comments-loader');
+
   commentsList.textContent = '';
+  let displayedComments = 0;
 
-  photo.comments.forEach((comment) => {
-    const commentItem = document.createElement('li');
-    commentItem.classList.add('social__comment');
+  const renderCommentsBatch = () => {
+    const nextCommentsBatch = photo.comments.slice(displayedComments, displayedComments + 5);
+    displayedComments += nextCommentsBatch.length;
 
-    const commentAvatar = document.createElement('img');
-    commentAvatar.classList.add('social__picture');
-    commentAvatar.src = comment.avatar;
-    commentAvatar.alt = comment.name;
-    commentAvatar.width = '35';
-    commentAvatar.height = '35';
+    nextCommentsBatch.forEach(({ avatar, name, message }) => {
+      const commentItem = document.createElement('li');
+      commentItem.classList.add('social__comment');
 
-    const commentText = document.createElement('p');
-    commentText.classList.add('social__text');
-    commentText.textContent = comment.message;
+      const commentAvatar = document.createElement('img');
+      commentAvatar.classList.add('social__picture');
+      commentAvatar.src = avatar;
+      commentAvatar.alt = name;
+      commentAvatar.width = 35;
+      commentAvatar.height = 35;
 
-    commentItem.append(commentAvatar, commentText);
-    commentsList.append(commentItem);
-  });
+      const commentText = document.createElement('p');
+      commentText.classList.add('social__text');
+      commentText.textContent = message;
+
+      commentItem.append(commentAvatar, commentText);
+      commentsList.append(commentItem);
+
+      if (displayedComments >= photo.comments.length) {
+        loadMoreButton.classList.add('hidden');
+      } else {
+        loadMoreButton.classList.remove('hidden');
+      }
+    });
+
+    const currentCount = Math.min(displayedComments, photo.comments.length);
+    commentCount.firstChild.textContent = `${currentCount} из `;
+  };
+
+  loadMoreButton.addEventListener('click', renderCommentsBatch);
+  renderCommentsBatch();
 };
