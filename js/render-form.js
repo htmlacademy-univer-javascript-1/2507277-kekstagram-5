2d88d848246197d0936019c4b3d24a8cc5d79be0
Jewel <zensot1@yandex.ru>
2024-12-07 20:23:48 +0500

    Помощь друга

--- js/render-form.js
+++ js/render-form.js
@@ -1,16 +1,23 @@
 import { pristine, onInput, inputHashtag, inputComment } from './validation.js';
+import { resetScale, scaleUp, scaleDown } from './img-scale.js';
+import { resetEffects, updateEffect } from './img-effects.js';
 
 export const renderForm = () => {
   const fileInput = document.querySelector('.img-upload__input');
   const overlay = document.querySelector('.img-upload__overlay');
   const closeButton = document.querySelector('#upload-cancel');
   const form = document.querySelector('.img-upload__form');
+  const scaleUpBtn = document.querySelector('.scale__control--bigger');
+  const scaleDownBtn = document.querySelector('.scale__control--smaller');
+  const effectRadios = document.querySelectorAll('.effects__radio');
 
   const closeModal = () => {
     overlay.classList.add('hidden');
     document.body.classList.remove('modal-open');
     form.reset();
     pristine.reset();
+    resetScale();
+    resetEffects();
   };
 
   fileInput.addEventListener('change', () => {
@@ -33,6 +40,16 @@ export const renderForm = () => {
     }
   });
 
+  effectRadios.forEach((radio) => {
+    radio.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        updateEffect(e.target.value);
+      }
+    });
+  });
+
   inputHashtag.addEventListener('input', onInput);
   inputComment.addEventListener('input', onInput);
+  scaleDownBtn.addEventListener('click', scaleDown);
+  scaleUpBtn.addEventListener('click', scaleUp);
 };
